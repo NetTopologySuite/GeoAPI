330e437feb166d2e066c19bebf330cd5400a9beb
Felix Obermaier <felix.obermaier@netcologne.de>
2015-11-16 11:53:17 +0100

    Cleanup

--- GeoAPI/Properties/AssemblyInfo.cs
+++ GeoAPI/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GeoAPI")]
-[assembly: AssemblyDescription("GeoAPI Version that matches NTS v.1.13.4")]
+[assembly: AssemblyDescription("GeoAPI Version that matches NTS v.1.14")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
@@ -11,13 +11,13 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: AssemblyCompany("NetTopologySuite - Team")]
 [assembly: AssemblyProduct("GeoAPI")]
-[assembly: AssemblyCopyright("Copyright ©  2007-2014")]
+[assembly: AssemblyCopyright("Copyright ©  2007-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 #if !PCL
 [assembly: ComVisible(false)]
 [assembly: Guid("b6726fc4-0319-4a6d-84f5-aafc6ba530e3")]
 #endif
-[assembly: AssemblyVersion("1.7.4.0")]
+[assembly: AssemblyVersion("1.7.4")]
 //[assembly: AssemblyFileVersion"1.5.0.0")]
 [assembly: CLSCompliant(true)]
