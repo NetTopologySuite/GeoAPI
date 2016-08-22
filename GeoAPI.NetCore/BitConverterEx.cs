b52ede15873fd4a8b9865074f99e57ba4d50097a
Joe Amenta <airbreather@linux.com>
2016-08-22 09:07:16 -0400

    Getting rid of the inefficient BitConverterEx. GetHashCode methods, especially for potentially "hot" types like Coordinate, should not allocate new objects on the heap if they can help it, for performance reasons.  Especially not two (in the case of Coordinate) or four (in the case of Envelope) such objects.  Each call to one of the two BitConverterEx methods allocated a new byte array. Turns out that this was only used in two places, and those two places only used it to reimplement System.Double.GetHashCode exactly as it's written in the BCL, so we can just call System.Double.GetHashCode directly. Alternatives would be to inline the definitions of the two methods (requires unsafe code) or to make a "union"-like LayoutKind.Explicit struct that has two members: one of type "double" and one of type "long", both declared as FieldOffset(0) (very annoying to look at).

--- GeoAPI.NetCore/BitConverterEx.cs
+++ GeoAPI.NetCore/BitConverterEx.cs
@@ -1,36 +1,5 @@
-﻿using System;
-
-namespace GeoAPI
+﻿namespace GeoAPI
 {
-    /// <summary>
-    /// A framework replacement for the System.BitConverter class
-    /// </summary>
-    /// <remarks>Only partial functionality is provided!</remarks>
-    internal static class BitConverterEx
-    {
-        /// <summary>
-        /// Function to convert the bits of a double to a the bits of a long
-        /// </summary>
-        /// <param name="self">The double value</param>
-        /// <returns>The long value</returns>
-        public static Int64 DoubleToInt64Bits(double self)
-        {
-            var tmp = BitConverter.GetBytes(self);
-            return BitConverter.ToInt64(tmp, 0);
-        }
-
-        /// <summary>
-        /// Function to convert the bits of a long to a the bits of a double
-        /// </summary>
-        /// <param name="self">The long value</param>
-        /// <returns>The double value</returns>
-        public static Double Int64BitsToDouble(Int64 self)
-        {
-            var tmp = BitConverter.GetBytes(self);
-            return BitConverter.ToDouble(tmp, 0);
-        }
-    }
-
     /// <summary>
     /// A framework replacement for the System.ICloneable interface.
     /// </summary>
