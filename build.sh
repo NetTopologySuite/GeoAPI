cdeb340eecbaf3418e894559b442a66dc3f6f50f
xivk <ben@itinero.tech>
2017-08-17 19:51:11 +0200

    Removed xbuild for now.

--- build.sh
+++ build.sh
@@ -5,13 +5,14 @@ set -e
 
 dotnet --version
 dotnet restore
-dotnet build ./src/GeoAPI
+dotnet build ./src/GeoAPI -f netstandard1.0
 
+# TODO: replace this with the Mono 5.0 mbuild.
 #
 # Build the project
 #
-xbuild /p:Configuration=Release /t:"Build" GeoAPI.sln /v:minimal
+# xbuild /p:Configuration=Release /t:"Build" GeoAPI.sln /v:minimal
 #
 # Run unit tests
 #
-mono .testRunner/NUnit.ConsoleRunner.3.6.0/tools/nunit3-console.exe ./Release/v4.5/AnyCPU/GeoAPI.Tests.dll 
+# mono .testRunner/NUnit.ConsoleRunner.3.6.0/tools/nunit3-console.exe ./Release/v4.5/AnyCPU/GeoAPI.Tests.dll 
