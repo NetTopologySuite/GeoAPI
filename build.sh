2a18ccaf21871fe0104d24d834429433e7175527
Joe Amenta <airbreather@linux.com>
2017-10-23 22:11:09 -0400

    Bring those comments back

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@
 #exit if any command fails
 set -e
 
+# Build the project
+#
 msbuild GeoAPI.sln "/t:Restore;Build" /p:Configuration=Release "/p:FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/" /v:minimal /p:WarningLevel=3
+
+# Run unit tests
+#
 mono .testRunner/NUnit.ConsoleRunner.3.6.0/tools/nunit3-console.exe ./test/GeoAPI.Tests/bin/Release/net45/GeoAPI.Tests.dll
