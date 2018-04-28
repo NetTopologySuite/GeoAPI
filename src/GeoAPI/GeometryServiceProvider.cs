5020f3c273570ec15eebc5dad7aac27414d292cd
Yoh Deadfall <yoh.deadfall@hotmail.com>
2018-04-28 23:54:45 +0300

    Improved performance of the Instance property (#39)
    * Improved performance of the Instance property
    
    * Feedback
    
    * Removed lock in setter
    
    * Removed reference to System.Reflection.Emit

--- src/GeoAPI/GeometryServiceProvider.cs
+++ src/GeoAPI/GeometryServiceProvider.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+#if COMPAT_BOOTSTRAP_USING_REFLECTION && HAS_SYSTEM_APPDOMAIN_GETASSEMBLIES && HAS_SYSTEM_REFLECTION_ASSEMBLY_GETEXPORTEDTYPES
 using System.Reflection;
+using System.Threading;
+#endif
 
 namespace GeoAPI
 {
@@ -9,90 +11,62 @@ namespace GeoAPI
     /// </summary>
     public static class GeometryServiceProvider
     {
-        private static IGeometryServices _instance;
-        private static readonly object LockObject = new object();
+        private static volatile IGeometryServices s_instance;
+        private static readonly object s_lock = new object();
 
         /// <summary>
         /// Gets or sets the <see cref="IGeometryServices"/> instance.
         /// </summary>
         public static IGeometryServices Instance
         {
-            get
-            {
-                lock (LockObject)
-                {
-                    return _instance ?? (_instance = ReflectInstance());
-                }
-            }
-
-            set
-            {
-                if (value == null)
-                    throw new ArgumentNullException("value", "You must not assign null to Instance!");
-
-                lock (LockObject)
-                {
-                    _instance = value;
-                }
-            }
+            get => s_instance ?? InitializeInstance();
+            set => s_instance = value ?? throw new ArgumentNullException(nameof(value));
         }
 
-#if HAS_SYSTEM_REFLECTION_ASSEMBLY_GETEXPORTEDTYPES
-        private static IEnumerable<Type> GetLoadableTypes(Assembly assembly)
+        private static IGeometryServices InitializeInstance()
         {
-            if (assembly == null)
-                return new Type[0];
+#if COMPAT_BOOTSTRAP_USING_REFLECTION && HAS_SYSTEM_APPDOMAIN_GETASSEMBLIES && HAS_SYSTEM_REFLECTION_ASSEMBLY_GETEXPORTEDTYPES
+            lock (s_lock)
+            {
+                var instance = s_instance;
+                if (instance != null) return instance;
+                foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    if (assembly.GlobalAssemblyCache && assembly.CodeBase == Assembly.GetExecutingAssembly().CodeBase)
+                        continue;
 
-            try
-            {
-                return assembly.GetExportedTypes();
-            }
-            catch (ReflectionTypeLoadException ex)
-            {
-                var types = ex.Types;
-                IList<Type> list = new List<Type>(types.Length);
-                foreach (var t in types)
-                    if (t != null && t.IsPublic)
+                    var assemblyType = assembly.GetType().FullName;
+                    if (assemblyType == "System.Reflection.Emit.AssemblyBuilder" ||
+                        assemblyType == "System.Reflection.Emit.InternalAssemblyBuilder")
+                        continue;
+
+                    Type[] types;
+
+                    try
                     {
-                        list.Add(t);
+                        types = assembly.GetExportedTypes();
                     }
-                return list;
-            }
-            catch
-            {
-                return new Type[0];
-            }
-        }
-#else
-        private static IEnumerable<Type> GetLoadableTypes(Assembly assembly)
-        {
-            throw new NotSupportedException("The compact framework does not support retrieve exported types from assembly");
-        }
-#endif
-
-        private static IGeometryServices ReflectInstance()
-        {
-#if COMPAT_BOOTSTRAP_USING_REFLECTION && HAS_SYSTEM_APPDOMAIN_GETASSEMBLIES
-            var a = AppDomain.CurrentDomain.GetAssemblies();
-            foreach (var assembly in a)
-            {
-                // Take a look at issue 114: http://code.google.com/p/nettopologysuite/issues/detail?id=114
-                if (assembly is System.Reflection.Emit.AssemblyBuilder) continue;
-                if (assembly.GetType().FullName == "System.Reflection.Emit.InternalAssemblyBuilder") continue;
-                if (assembly.GlobalAssemblyCache && assembly.CodeBase == Assembly.GetExecutingAssembly().CodeBase) continue;
-
-                foreach (var t in GetLoadableTypes(assembly))
-                {
-                    if (t.IsInterface) continue;
-                    if (t.IsAbstract) continue;
-                    if (t.IsNotPublic) continue;
-                    if (!typeof(IGeometryServices).IsAssignableFrom(t)) continue;
-
-                    var constuctors = t.GetConstructors();
-                    foreach (var constructorInfo in constuctors)
+                    catch (ReflectionTypeLoadException ex)
                     {
-                        if (constructorInfo.IsPublic && constructorInfo.GetParameters().Length == 0)
-                            return (IGeometryServices)Activator.CreateInstance(t);
+                        types = ex.Types;
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
+
+                    var requiredType = typeof(IGeometryServices);
+                    foreach (var type in types)
+                    {
+                        if (type.IsNotPublic || type.IsInterface || type.IsAbstract || !requiredType.IsAssignableFrom(type))
+                            continue;
+
+                        foreach (var constructor in type.GetConstructors())
+                            if (constructor.IsPublic && constructor.GetParameters().Length == 0)
+                            {
+                                Interlocked.CompareExchange(ref s_instance, (IGeometryServices)Activator.CreateInstance(type), null);
+                                return s_instance;
+                            }
                     }
                 }
             }
